b592321ef3d3b8fefe79182a250e2c3721f4c6f6
Abdullah Alharbi <alharbi.s.abdullah@gmail.com>
2026-08-27 21:12:13 +0300

    theme: add solitude layer (starship palette + 4 dharmx wallpapers)

---
+++ common/themes/solitude/theme.lua
@@ -0,0 +1,22 @@
+return {
+  name = "solitude",
+  border = "0xff798186",
+  macos = "dark",
+  vscode = "Noctokai",
+  vscode_ext = "farigab.noctokai-theme",
+  cursor = "Solitude (generated)",
+  colors = {
+    background = "#101315",
+    foreground = "#cacccc",
+    cursor_bg = "#cacccc",
+    cursor_fg = "#101315",
+    selection_bg = "#343d41",
+    selection_fg = "#cacccc",
+    ansi = {
+      "#101315", "#565d60", "#9fa5a9", "#d9dbdc", "#798186", "#aeaeae", "#707070", "#cbc2be",
+    },
+    brights = {
+      "#4b4e55", "#de6145", "#343d41", "#c9c2b4", "#5d6367", "#9a9a9a", "#707070", "#a5aeb4",
+    },
+  },
+}
